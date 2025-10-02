7038ccd84146407d6b66959f3376b017a2956ed9
Vilberto02 <vilberto.patricio@unmsm.edu.pe>
2025-10-02 15:28:38 -0500

    Configurar tipado de algunos componentes iniciales

--- source/frontend/src/types/types.ts
+++ source/frontend/src/types/types.ts
@@ -1,3 +1,20 @@
-export interface Prueba {
-  ejemplo: string;
-}
+export type LoginFields = {
+  email: string;
+  password: string;
+};
+
+export type RegisterInfoFields = {
+  name: string;
+  lastname: string;
+  number_document: string;
+  number_phone: number;
+  name_business: string;
+  address: string;
+  number_ruc: string;
+};
+
+export type RegisterCredentialFields = LoginFields & {
+  confirm_password: string;
+};
+
+export type FormData = RegisterInfoFields & RegisterCredentialFields;
